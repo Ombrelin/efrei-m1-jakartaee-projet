aea74773d27c287308dc86fb072e8706e0ec7ae3
JM REMEUR <jm@remeur.fr>
2020-11-17 22:11:16 +0100

    get matches

--- src/main/java/fr/mjta/tenis/domain/repositories/MatchRepositoryImpl.java
+++ src/main/java/fr/mjta/tenis/domain/repositories/MatchRepositoryImpl.java
@@ -1,10 +1,13 @@
 package fr.mjta.tenis.domain.repositories;
 
 import fr.mjta.tenis.domain.entities.Match;
+import fr.mjta.tenis.domain.entities.Organizer;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
 
 @Stateless
 public class MatchRepositoryImpl implements MatchRepository {
@@ -16,5 +19,15 @@ public class MatchRepositoryImpl implements MatchRepository {
     public Match insertMatch(Match match) {
         this.entityManager.persist(match);
         return match;
+    }
+
+    @Override
+    public List<Match> getMatches() {
+        return entityManager.createQuery("SELECT m FROM Match m", Match.class).getResultList();
+    }
+
+    @Override
+    public List<Match> getFinishedMatches() {
+        return entityManager.createQuery("SELECT m FROM Match m", Match.class).getResultList();
     }
 }
--- src/main/java/fr/mjta/tenis/domain/services/MatchService.java
+++ src/main/java/fr/mjta/tenis/domain/services/MatchService.java
@@ -4,8 +4,13 @@ import fr.mjta.tenis.domain.entities.Match;
 
 import javax.ejb.Local;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Local
 public interface MatchService {
     void planMatch(LocalDateTime dateTime, String court);
+
+    List<Match> getMatches();
+
+    List<Match> getFinishedMatches();
 }
