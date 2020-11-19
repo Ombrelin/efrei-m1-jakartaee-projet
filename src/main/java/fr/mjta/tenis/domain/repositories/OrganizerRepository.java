31d51c7c48c3136329a0ae2d2104be23423a870d
Arsène Lapostolet <arsene@lapostolet.fr>
2020-11-19 16:44:30 +0100

    fix login

--- src/main/java/fr/mjta/tenis/domain/repositories/OrganizerRepository.java
+++ src/main/java/fr/mjta/tenis/domain/repositories/OrganizerRepository.java
@@ -3,10 +3,11 @@ package fr.mjta.tenis.domain.repositories;
 import fr.mjta.tenis.domain.entities.Organizer;
 
 import javax.ejb.Local;
+import java.util.Optional;
 
 @Local
 public interface OrganizerRepository {
     Organizer getById(String id);
 
-    Organizer getByLogin(String login);
+    Optional<Organizer> getByLogin(String login);
 }
